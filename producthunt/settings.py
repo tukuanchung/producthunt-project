36ffcff6c25f4a10499a6fd5cc77ec3247163920
kuanchung tu <tukuanchung@gmail.com>
2018-07-04 14:45:07 +0800

    Product create

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -128,6 +128,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'producthunt/static/')
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- producthunt/urls.py
+++ producthunt/urls.py
@@ -2,12 +2,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from products import views
+from django.conf import settings
+from django.conf.urls.static import static
 # from accounts import views as accounts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home' ),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('products/', include('products.urls'))
     # path('signup', accounts_views.signup, name='signup'),
     # path('login', accounts_views.login, name='login'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
